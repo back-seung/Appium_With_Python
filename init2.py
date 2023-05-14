ad28a6ef509de043c1bc973e3e8d31d4ba5089e8
back-seung <skud113@naver.com>
2023-05-15 00:59:35 +0900

    [Add] final test code & Record File(.mp4) add

--- init2.py
+++ init2.py
@@ -1,5 +1,4 @@
-import time
-
+from typing import Optional
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
@@ -15,48 +14,13 @@ import configs.driver
 import utility.lorem as lorem
 
 
-def extract_coordinates(bounds: str) -> tuple:
-    """
-    extract
-    :param bounds:
-    :return:
-    """
-    coords = bounds.replace("][", ",").strip("[]").split(",")
-    x_start, y_start, x_end, y_end = map(int, coords)
-
-    return x_start, y_start, x_end, y_end
-
-
-def tab_element(element):
-    """
-    Do 'DEVICE TAB' from Element's bounds
-    :param element: Target of DEVICE TAB
-    """
-    x_start, y_start, x_end, y_end = extract_coordinates(str(element.get_attribute("bounds")))
-
-    x = (x_start + x_end) // 2
-    y = (y_start + y_end) // 2
-
-    touch_action = TouchAction(driver)
-    touch_action.tap(None, x, y).perform()
-
-def scroll_down(element):
-    x_start, y_start, x_end, y_end = extract_coordinates(str(element.get_attribute("bounds")))
-
-    x = (x_start + x_end) // 2
-    y = (y_start + y_end) // 2
-
-    touch_action = TouchAction(driver)
-    tab_element(element)
-    touch_action.long_press(element).move_to(0, random.randint(1, 99)).release().perform()
-
 def find_element(by_val: str, dest: str, is_multiple: bool):
     """
-    element will presence located, return this element
-    :param by_val: By.ID, By.XPATH, By.CLASS ...more
-    :param dest: destination of By
-    :param is_multiple: for return object OR list
-    :return: element
+    find element by given parameters.
+    :param by_val: value of By
+    :param dest: destination
+    :param is_multiple: find element is single or multiple
+    :return:
     """
     by = {"ID": By.ID, "XPATH": By.XPATH, "CLASS": By.CLASS_NAME}
     by_type = by.get(by_val)
@@ -74,13 +38,14 @@ def find_element(by_val: str, dest: str, is_multiple: bool):
 
     return element
 
+
 def input_textbox(element: object, text: str):
     """
-    Set the textbox with given text
-    :param element: target, it must be text box element
-    :param text: text box will be set from this given text
+    send keys into textbox.
+    :return: text
     """
     WebDriverWait(driver, 10).until(ec.element_to_be_clickable(element))
+    element.clear()
     element.send_keys(text)
 
     if not element.get_attribute("text") == str(text):
@@ -92,6 +57,10 @@ def input_textbox(element: object, text: str):
 
 
 def extract_rgb(element):
+    """
+    extract R,G,B from given element
+    :return: list of RGB
+    """
     size = element.size
     location = element.location
 
@@ -102,12 +71,26 @@ def extract_rgb(element):
     return [color[0], color[1], color[2]]
 
 
+def tab_device_with_bounds(bounds: str) -> tuple:
+    """
+    tab the device using with given bounds
+    :param bounds: e.g [123,123][12,345]
+    """
+    coords = bounds.replace("][", ",").strip("[]").split(",")
+    x_start, y_start, x_end, y_end = map(int, coords)
+
+    x = (x_start + x_end) // 2
+    y = (y_start + y_end) // 2
+
+    touch_action = TouchAction(driver)
+    touch_action.tap(None, x, y).perform()
+
+
 def set_alarm(ampm: str, save_or_not: bool):
     """
-    Set the alarm with given params. time is always 8:00 fixed
-    :param ampm:
-    :param save_or_not:
-    :return:
+    set the alarm. time is fixed at (8:00) because cant handle this.
+    :param ampm: save time is AM or PM
+    :return: get text attribute from elemenmt
     """
     button = find_element("ID", "reminderTimePicker", False)
     button.click()
@@ -127,12 +110,11 @@ def set_alarm(ampm: str, save_or_not: bool):
 
 def set_frequency(fre: str, type: str, value: int):
     """
-    1. Extract frequency list
-    2. Click the Frequency button
-    3. set Frequency, and set value
-    4. Save
-    :param fre: standard of frequency
-    :param value: input value of element
+
+    :param fre: "DAY", "WEEK", "MONTH" only
+    :param type: yesNo Type or Measurable Type
+    :param value: frequency value
+    :return: str(fre + " : " + str(value))  -> e.g. "WEEK : 4 times per week"
     """
     button_diff = {"buttonYesNo": "boolean_frequency_picker", "buttonMeasurable": "numericalFrequencyPicker"}
 
@@ -180,24 +162,38 @@ def set_frequency(fre: str, type: str, value: int):
     return str(fre + " : " + str(value))
 
 
-def save_habit(type: str):
+def save_or_modify_habit(save_or_modify: str, title_name: Optional[str], save_type: Optional[str]) -> None:
     """
-
-    :param type: "buttonYesNo", "buttonMeasurable"
-    :return:
+    saving habit or modifying habbit.
+    :param save_or_modify: is INSERT or MODIFY
+    :param title_name: MODIFY only -> title of element
+    :param save_type: is yesNo Type or Measurable Type
+    :return: list of saving element's value
     """
-    # 입력값 저장
+    # 입력값 저장 용도 list
     list = []
+    fre_arr = ["DAY", "WEEK", "MONTH"]
+    am_or_pm = ["AM", "PM"]
+    set_or_not = [True, False]
 
-    # 타입에 따라 습관 등록 버튼 클릭
-    find_element("ID", "actionCreateHabit", False).click()
-    find_element("ID", type, False).click()
+    # 등록 또는 수정 구분
+    if save_or_modify == "INSERT":
+        print("#####  습관 추가 시작  #####")
+        find_element("ID", "actionCreateHabit", False).click()
+        find_element("ID", save_type, False).click()
+        # 빈도 설정
+        list.append(set_frequency(fre_arr[random.randint(0, (len(fre_arr) - 1))], save_type, random.randint(1, 10)))
+    elif save_or_modify == "MODIFY":
+        print("#####  습관 수정 시작  #####")
+        find_element("XPATH", "//*[@text='" + title_name + "']", False).click()
+        find_element("ID", "action_edit_habit", False).click()
+        # 빈도 설정
+        list.append(set_frequency(fre_arr[random.randint(0, (len(fre_arr) - 1))], save_type, random.randint(1, 10)))
 
-    # 각 타입의 공통된 Parameter
     # 제목 입력 및 저장
     list.append(input_textbox(find_element("ID", "nameInput", False), lorem.words(3)))
 
-    # 컬러 선택 및 RGB 비교 검증
+    # 컬러 선택 및 RGB 저장
     color_btn = find_element("ID", "colorButton", False)
     color_btn.click()
     random_idx = random.randint(1, 20)
@@ -207,39 +203,58 @@ def save_habit(type: str):
     picked_color = find_element("XPATH",
                                 '//android.widget.FrameLayout[@content-desc="Color ' + str(random_idx) + ' selected"]',
                                 False)
-
     if picked_color is not None:
         picked_color.click()
         color2 = extract_rgb(find_element("ID", "colorButton", False))
         if not color1 == color2:
-            print("   ㄴ[컬러 설정] - FAIL ", str(random_idx), " -> 설정 값으로 반영 되지 않았습니다.")
+            print("   ㄴ[컬러 설정] - FAIL \"", str(random_idx), "\" -> 설정 값으로 반영 되지 않았습니다.")
         else:
-            print("   ㄴ[컬러 설정] - PASS ", str(random_idx), " -> 설정 값으로 반영 되었습니다.")
+            print("   ㄴ[컬러 설정] - PASS \"", str(random_idx), "\" -> 설정 값으로 반영 되었습니다.")
 
     # 반영된 색 저장
     list.append(color2)
-
     # 질문 입력 및 저장
     list.append(input_textbox(find_element("ID", "questionInput", False), lorem.words(3)))
 
-    # 빈도 설정
-    list.append(set_frequency("DAY", type, random.randint(1, 10)))
-
     # 알림 설정 및 결과 저장
-    am_or_pm = ["AM", "PM"]
-    list.append(set_alarm(am_or_pm[random.randint(0, 1)], True))
+    list.append(set_alarm(am_or_pm[random.randint(0, (len(am_or_pm) - 1))],
+                          set_or_not[random.randint(0, (len(set_or_not) - 1))]))
 
-    # 메모 입력 및 저장
-    list.append(input_textbox(find_element("ID", "notesInput", False), lorem.words(3)))
-
-    if type == "buttonMeasurable":
+    if save_type == "buttonMeasurable":
         list.append(input_textbox(find_element("ID", "unitInput", False), lorem.words(1)))
         list.append(input_textbox(find_element("ID", "targetInput", False), random.randint(1, 15)))
 
+    # 메모 입력 및 저장
+    list.append(input_textbox(find_element("ID", "notesInput", False), lorem.words(3)))
     # 정의한 습관 저장
     find_element("ID", "buttonSave", False).click()
 
+    # 수정시 사용
+    if save_type is not None:
+        list.append(save_type)
+
+    if save_or_modify == "MODIFY":
+        find_element("XPATH", '//android.widget.ImageButton[@content-desc="위로 이동"]', False).click()
+        print("#####  습관 수정 종료  #####")
+    else:
+        print("#####  습관 추가 종료  #####")
+
     return list
+
+
+def del_habit(title_name: str):
+    """
+    deleting habit.
+    :param title_name: title name of habit
+    """
+    print("## 습관 삭제 시작")
+
+    find_element("XPATH", "//*[@text='" + title_name + "']", False).click()
+    tab_device_with_bounds("[649,75][720,159]")
+    find_element("XPATH", "//*[@text='삭제']", False).click()
+    find_element("XPATH", "//*[@text='네']", False).click()
+
+    print("## 습관 삭제 완료")
 
 
 # Device Remote 시작
@@ -260,36 +275,43 @@ for i in range(2):
     find_element("ID", "next", False).click()
 find_element("ID", "done", False).click()
 
+saved_arr = []
 # 습관 추가 - 예 아니오 기준
-saved_habit_yes_or_no = save_habit("buttonYesNo")
-print(saved_habit_yes_or_no)
+saved_arr.append(save_or_modify_habit("INSERT", None, "buttonYesNo"))
 # 습관 추가 - 측정 가능 기준
-saved_habit_measurable = save_habit("buttonMeasurable")
-print(saved_habit_measurable)
+saved_arr.append(save_or_modify_habit("INSERT", None, "buttonMeasurable"))
 
-# 습관 수정 - 임의의 습관
-habits = [saved_habit_yes_or_no, saved_habit_measurable]
+# 습관 기록 확인
+habits = saved_arr
 for habit in habits:
     for i in range(5):
         box = find_element("XPATH",
                            "(//*[@class='android.widget.LinearLayout' and (./preceding-sibling::* | ./following-sibling::*)[@text='" +
                            habit[
-                               0] + "'] and ./parent::*[@class='android.widget.LinearLayout'] and (./preceding-sibling::* | ./following-sibling::*)[@class='android.view.View']]/*[@class='android.view.View'])[" + str(
+                               1] + "'] and ./parent::*[@class='android.widget.LinearLayout'] and (./preceding-sibling::* | ./following-sibling::*)[@class='android.view.View']]/*[@class='android.view.View'])[" + str(
                                i + 1) + "]",
                            False)
         action = TouchAction(driver)
         WebDriverWait(driver, 10).until(ec.element_to_be_clickable(box))
-        action.long_press(box).wait(2000).perform()
+        action.long_press(box).perform()
 
-        if len(habit) > 6:
+        if len(habit) > 8:
             scrolls = find_element("XPATH", "//*[@resource-id='android:id/numberpicker_input']", True)
-            print(len(scrolls))
             for scroll in scrolls:
                 WebDriverWait(driver, 10).until(ec.element_to_be_clickable(scroll))
                 scroll.clear()
                 scroll.send_keys(str(random.randint(0, 100)))
             find_element("XPATH", "//*[@text='확인']", False).click()
-    # 측정 가능값 체크
+
+# 습관 수정
+modified_arr = []
+for saved in habits:
+    print(saved)
+    modified_arr.append(save_or_modify_habit("MODIFY", saved[1], saved[len(saved) - 1]))
+
+# 습관 삭제
+for saved in modified_arr:
+    del_habit(saved[1])
 
 # 녹화 종료 -> Base64로 디코딩
 recording_data = driver.stop_recording_screen()
